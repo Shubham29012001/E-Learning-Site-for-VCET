331297715aae75666d2c64bc77ba5235a9bf6759
Shubham29012001 <smmaurya29012001@gmail.com>
2022-12-25 13:53:45 +0530

    Adding Redis Messaging Service with Render Redis Instance

--- app.js
+++ app.js
@@ -66,108 +66,105 @@ app.use(
   )
 );
 
-// io.on('connect', (socket) => {
+io.on('connect', (socket) => {
 
-//   socket.on('join', ({ UserName, room, userId }, callback) => {
-//     console.log(UserName, room, userId);
-//     let newUser = false;
-//     let users = [{ id: [], names: [] }];
+  socket.on('join', ({ UserName, room, userId }, callback) => {
+    let newUser = false;
+    let users = [{ id: [], names: [] }];
 
-//     if (client.EXISTS(room)) {
-//       client.lrange(room, 0, -1, function (err, result) {
-//         if (err) {
-//           callback(err);
-//         }
-//         else {
-//           console.log("lrange result join=", result);
-//           const History = [];
+    if (client.EXISTS(room)) {
+      client.lrange(room, 0, -1, function (err, result) {
+        if (err) {
+          callback(err);
+        }
+        else {
+          const History = [];
 
-//           result.forEach(user => {
-//             user = JSON.parse(user)
-//             History.push(user)
+          result.forEach(user => {
+            user = JSON.parse(user)
+            History.push(user)
 
-//             if (!users[0].id.includes(user.userId)) {
-//               users[0].id.push(user.userId);
-//               users[0].names.push(user.UserName);
-//               console.log("user added to list")
-//             }
+            if (!users[0].id.includes(user.userId)) {
+              users[0].id.push(user.userId);
+              users[0].names.push(user.UserName);
+            }
 
-//           })
-//           console.log(users)
+          })
+          console.log(users)
 
-//           if (!users[0].id.includes(userId)) {
-//             newUser = true;
-//             console.log("userUser")
-//             users[0].id.push(userId);
-//             users[0].names.push(UserName);
+          if (!users[0].id.includes(userId)) {
+            newUser = true;
+            console.log("userUser")
+            users[0].id.push(userId);
+            users[0].names.push(UserName);
 
-//           }
+          }
 
-//           socket.emit('history', { History: History, users: users[0].names });
-//           socket.join(room)
-//           io.to(room).emit('admin',
-//             {
-//               users: users[0].names,
-//               UserName: 'admin',
-//               newUser: newUser,
-//               Message: newUser ? `Welcome to the class ${UserName}!` : `Welcome back to the class ${UserName}!`
-//             });
+          socket.emit('history', { History: History, users: users[0].names });
+          socket.join(room)
+          io.to(room).emit('admin',
+            {
+              users: users[0].names,
+              UserName: 'admin',
+              newUser: newUser,
+              Message: newUser ? `Welcome to the class ${UserName}!` : `Welcome back to the class ${UserName}!`
+            });
 
-//           socket.broadcast.to(room).emit('admin', { users: users, UserName: `${UserName}`, users: users[0].names, newUser: newUser, Message: `${UserName} has joined!` });
-//           newUser = false;
-//         }
-//       });
-//     }
-//     else {
-//       client.hset(room, null, function (err, result) {
-//         if (err)
-//           callback(err);
-//         else {
-//           console.log("setting redis hset::", result)
-//         }
-//       });
-//     }
+          socket.broadcast.to(room).emit('admin', { users: users, UserName: `${UserName}`, users: users[0].names, newUser: newUser, Message: `${UserName} has joined!` });
+          newUser = false;
+        }
+      });
+    }
+    else {
+      client.hset(room, null, function (err, result) {
+        if (err)
+          callback(err);
+        else {
+          console.log("setting redis hset::", result)
+        }
+      });
+    }
 
-//     callback()
-//   })
+    callback()
+  })
 
-//   socket.on('sendMessage', ({ UserName, userId, room, message }, callback) => {
+  socket.on('sendMessage', ({ UserName, userId, room, message }, callback) => {
 
-//     const user = { "UserName": UserName, "Message": message, "userId": userId }
+    const user = { "UserName": UserName, "Message": message, "userId": userId }
 
-//     if (client.EXISTS(room)) {
-//       client.rpush(room, JSON.stringify(user), function (err, result) {
-//         if (err)
-//           callback(err)
-//         else {
-//           console.log("rpush::", result)
-//         }
-//       });
+    if (client.EXISTS(room)) {
+      client.rpush(room, JSON.stringify(user), function (err, result) {
+        if (err)
+          callback(err)
+        else {
+          console.log("rpush::", result)
+        }
+      });
 
-//     } else {
-//       client.hset(room, JSON.stringify(user), function (err, result) {
-//         if (err)
-//           callback(err)
-//         else {
-//           console.log("hset::", result)
-//         }
-//       });
-//     }
-//     client.lrange(room, 0, -1, function (err, result) {
-//       console.log("redis result=", result)
-//       if (err) {
-//         console.log(err)
-//       }
-//     })
-//     console.log(`${room} message sent by ${UserName} is::`, message);
-//     io.to(room).emit('Received_message', { UserName: UserName, Message: message });
-//     callback()
-//   })
+    } else {
+      client.hset(room, JSON.stringify(user), function (err, result) {
+        if (err)
+          callback(err)
+        else {
+          console.log("hset::", result)
+        }
+      });
+    }
+    client.lrange(room, 0, -1, function (err, result) {
+      console.log("redis result=", result)
+      if (err) {
+        console.log(err)
+      }
+    })
+    console.log(`${room} message sent by ${UserName} is::`, message);
+    io.to(room).emit('Received_message', { UserName: UserName, Message: message });
+    callback()
+  })
 
-//   socket.on('disconnect', () => {
-//     console.log('disconnected')
-//   })
-// })
+  socket.on('disconnect', () => {
+    console.log('disconnected')
+  })
+})
 
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
